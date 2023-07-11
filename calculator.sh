be6e797044cf80ff692a11f251986c95fe024554
Mirkoanonimo <138584714+Mirkoanonimo@users.noreply.github.com>
2023-07-11 16:10:32 +0200

    Create calculator.sh

---
+++ calculator.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Esegue l'operazione corretta
+if [ "$1" == "+" ]; then
+    ./add.sh $2 $3
+elif [ "$1" == "-" ]; then
+    ./sub.sh $2 $3
+elif [ "$1" == "*" ]; then
+    ./mul.sh $2 $3
+elif [ "$1" == "/" ]; then
+    ./div.sh $2 $3
+else
+    echo "Operatore non riconosciuto"
+fi
